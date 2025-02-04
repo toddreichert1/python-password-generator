de3002806d7f84dc118ae53ceb6bcbd9b346c7e7
Todd Reichert <toddreichert@mac.com>
2025-02-04 14:47:35 -0600

    added validation test

--- test_info.py
+++ test_info.py
@@ -2,11 +2,19 @@
 
 import pytest
 import subprocess
+from info import *
 
 # create and retrieve a password
-def test_system_command():
+def test_creation_and_retrieval():
     subprocess.run(["python3", "info.py", "-p", "1234"], capture_output=True, text=True)
     subprocess.run(["python3", "info.py", "-s", "1234"], capture_output=True, text=True)
     password = subprocess.run(["python3", "info.py", "-e", "ENV1"], capture_output=True, text=True)
     password2 = subprocess.run(["python3", "info.py", "-g", "ENV1"], capture_output=True, text=True)
     assert password.stdout == password2.stdout 
+
+# create and retrieve a password
+def test_password_validation():
+    subprocess.run(["python3", "info.py", "-p", "1234"], capture_output=True, text=True)
+    subprocess.run(["python3", "info.py", "-s", "1234"], capture_output=True, text=True)
+    password = subprocess.run(["python3", "info.py", "-e", "ENV2"], capture_output=True, text=True)
+    assert validate(password.stdout) == True 
